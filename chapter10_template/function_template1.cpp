c7799c43a8f0158d00550725e61f30261e37f4d1
SaubhagyaSapkota <saubhagyasapkota444@gmail.com>
2022-07-10 20:25:58 +0545

    a simple function template

--- chapter10_template/function_template1.cpp
+++ chapter10_template/function_template1.cpp
@@ -6,11 +6,13 @@ void testfunc(t1 a,t2 b)
 {
     cout<<a<<" "<<b<<endl;
 }
+
 int main()
 {
     int inum = 5;
     float fnum = 7.5;
     testfunc(inum,fnum);
     testfunc(20,"lets make this world a better place ");
+
     return 0;
 }
