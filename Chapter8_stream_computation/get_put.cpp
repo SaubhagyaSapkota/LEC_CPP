c6b59f3da6270f8907320dd152fb03af945bb10b
SaubhagyaSapkota <saubhagyasapkota444@gmail.com>
2022-07-20 11:20:36 +0545

    using get and put

---
+++ Chapter8_stream_computation/get_put.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char c,str[25];
+    cout<<"enter a character:";
+    cin.get(c);
+    cout<<"the entered character is :";
+    cout.put(c);
+    return 0;
+}
