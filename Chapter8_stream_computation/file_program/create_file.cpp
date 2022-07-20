519411e8dad5064ba1f5670dd7561e56358c7549
SaubhagyaSapkota <saubhagyasapkota444@gmail.com>
2022-07-20 12:51:49 +0545

    creating file

---
+++ Chapter8_stream_computation/file_program/create_file.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    fstream file;
+
+    file.open("createfile.txt",ios::out);
+    if (!file)
+    {
+        cout<<"error in creating file."<<endl;
+    }
+cout<<"file created successfully";
+file.close();
+return 0;
+}
